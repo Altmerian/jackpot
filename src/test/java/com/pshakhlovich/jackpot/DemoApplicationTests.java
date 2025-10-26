96ad5666c552e0eb2dc79a62040a2d46da35694a
Pavel Shakhlovich <altmersc@gmail.com>
2025-10-26 21:58:05 +0100

    init

---
+++ src/test/java/com/pshakhlovich/jackpot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pshakhlovich.jackpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
